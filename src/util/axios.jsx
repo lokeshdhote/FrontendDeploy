79b833a35a516e0bfc52adc7fc6fe6803d9c1cab
lokeshdhote <lokeshdhote2004@gmail.com>
2024-07-17 23:06:25 +0530

    n

--- src/util/axios.jsx
+++ src/util/axios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance =  axios.create({
-    baseURL :"https://backenddeploy-7n72.onrender.com/", 
+    baseURL :"https://backenddeploy-7n72.onrender.com/",
+     
     // baseURL :"http://localhost:3000/",
 
     withCredentials:true,
