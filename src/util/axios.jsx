ac08b2083c11211bdf748d76e5e58b8d5e5198df
lokeshdhote <lokeshdhote2004@gmail.com>
2024-10-25 00:32:16 +0530

    2

--- src/util/axios.jsx
+++ src/util/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance =  axios.create({
-    baseURL :"https://backend-deploy-blush.vercel.app/",
+    baseURL :"https://backend-deploy-blush.vercel.app",
 
     // baseURL :"http://localhost:3000/",
 
